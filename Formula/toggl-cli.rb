6c9365e3e640cbdbeea157a56ea744cb353be975
Shantanu Raj <s@sraj.me>
2023-09-07 00:30:32 +0200

    chore(release): toggl-cli v0.3.0

---
+++ Formula/toggl-cli.rb
@@ -0,0 +1,18 @@
+class TogglCli < Formula
+  desc "Unofficial CLI client for Toggl"
+  homepage "https://github.com/watercooler-labs/toggl-cli"
+  url "https://github.com/watercooler-labs/toggl-cli/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "81515d479495d6980edbe858c7f4f334e89b725ca0b84021cc367485d6a801a3"
+  license "MIT"
+  head "https://github.com/watercooler-labs/toggl-cli.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "-vv", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/toggl", "--help"
+  end
+end
